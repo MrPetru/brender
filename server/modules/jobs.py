e7ae58f71135b1a5c730830155f2e33d41b5d866
Scantlight <scantlight@gmail.com>
2014-01-22 19:30:07 +0100

    some strange things with repo source
    * should change in the future

--- server/modules/jobs.py
+++ server/modules/jobs.py
@@ -131,6 +131,7 @@ def start_job(worker, job):
               'start': job.chunk_start,
               'end': job.chunk_end,
               'repo_path': repo_path,
+              'server_repo_path': shot.path_server, 
               'rev': rev,
               'repo_type': repo_type}
 
